baefd211797d9fec34909e986c41a47d64007a1a
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-04-01 10:06:45 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,5 +2,15 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
-assert(factorial(10) === 3628800);
-assert(e(10) === 2.7182818011463845);
+function test(n) {
+    if(n === 0) return 1;
+    else return n * test(n - 1);
+}
+function euler(n) {
+    if(n === 0) return 1;
+    else return 1.0 / test(n) + e(n - 1);
+}
+
+for (let i = 0; i < 2000; i++) {
+    assert(e(i) === euler(i));
+}
