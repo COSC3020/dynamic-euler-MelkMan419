d64c66682a924f698c3e735f7c674250e4de4cce
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-04-01 10:03:13 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
-function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    let sum = 1;
+    let i = 1;
+    let curr = 1;
+    while (i <= n) {
+        curr *= i;
+        sum += 1 / curr;
+        i++;
+    }
+    return sum;
 }
